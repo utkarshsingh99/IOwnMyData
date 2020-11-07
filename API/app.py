fdb7b9f96fb08a89037281cca339ee088ae3a456
Prashant Sengar <prashantsengar5@gmail.com>
2020-11-07 22:18:47 +0530

    created api and moved to directories

---
+++ API/app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'well, you own it'
+
+@app.route('/reddit')
+def reddit():
+    username = request.args.get('username')
+    username = request.args.get('username')
+    return 'well, you own it'
+
+if __name__=='__main__':
+    app.run('localhost', port=1244, debug=True)
